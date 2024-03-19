bb86a8b8a3cbd364beb75fd76808df8755f52767
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2024-03-19 19:34:24 -0300

    save users in mempry

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
+
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usúarios')
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John',
+      email: 'john@example.com',
+    })
     return res.end('Criação de usúarios')
   }
 
